863593dd20e854d4683aa1f53c32803f71fe00c4
Maxim-Grek <127651643+Maxim-Grek@users.noreply.github.com>
2023-04-03 21:32:09 +0300

    Add files via upload

---
+++ 2 configuration.py
@@ -0,0 +1,10 @@
+user_token = '' # токен пользователя
+comm_token = '' # токен сообщества
+
+offset = 0 #смещение
+line = range(0, 1000) # последовательность для перебора найденных пользователей
+
+host = '127.0.0.1' 
+user = 'postgres'
+password = 'Maxim1983'
+db_name = 'bot_users'
